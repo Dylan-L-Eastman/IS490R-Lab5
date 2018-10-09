e18b9a129a279be1f617b9058df708f2869467ef
Ubuntu <ubuntu@ip-172-31-93-133.ec2.internal>
2018-10-09 16:05:38 +0000

    code deploy test.

--- home/views.py
+++ home/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world! You’re at the home index")
+    return HttpResponse("Hello, world! You’re at the home index. CodeDeploy working.")
 
 # Create your views here.
